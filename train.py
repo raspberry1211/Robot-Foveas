2af31ddc3cdd659251204be6826e4c955fd799b1
Dashiell <drfj2024@mymail.pomona.edu>
2025-04-30 19:43:34 -0700

    Bug fixes

--- train.py
+++ train.py
@@ -30,8 +30,8 @@ def main():
         transforms.RandomHorizontalFlip(),
         transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
         transforms.RandomRotation(15),
-        transforms.RandomErasing(p=0.5, scale=(0.02, 0.1), ratio=(0.3, 3.3)),
         transforms.ToTensor(),
+        transforms.RandomErasing(p=0.5, scale=(0.02, 0.1), ratio=(0.3, 3.3)),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                            std=[0.229, 0.224, 0.225])
     ])
