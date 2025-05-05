d44398c2c5332a055f8a27d7bc8130fa6e17c8a4
drfj2024 <drfj2024@gpu002.hpc.pomona.edu>
2025-05-05 14:48:17 -0700

    Fixed resize

--- train.py
+++ train.py
@@ -50,14 +50,14 @@ def main():
     # Modify the training transformation pipeline
     train_transform = transforms.Compose([
         transforms.TrivialAugmentWide(),
-        transforms.Resize((256, 256)),
+        transforms.Resize(256),
         transforms.RandomCrop((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     val_test_transform = transforms.Compose([
-        transforms.Resize((256, 256)),
+        transforms.Resize(256),
         transforms.CenterCrop((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
