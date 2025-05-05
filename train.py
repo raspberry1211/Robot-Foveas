fadf71874c9750d0c9a2cb9c1b5894992a6ad729
drfj2024 <drfj2024@gpu002.hpc.pomona.edu>
2025-05-05 14:52:53 -0700

    Fixed train transform to correctly match the paper

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ def main():
     train_transform = transforms.Compose([
         transforms.TrivialAugmentWide(),
         transforms.Resize(256),
-        transforms.RandomCrop((224, 224)),
+        transforms.RandomResizedCrop((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
